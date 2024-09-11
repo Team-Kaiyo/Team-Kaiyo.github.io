c5a25e35960ffbe5a9551ad32046ec435e2d1a13
Ratul Hasan <34002411+RaSan147@users.noreply.github.com>
2024-09-11 12:47:25 +0600

    Removing pwa

---
+++ copy_to_others.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+with open("index.html", "r", encoding="utf-8") as f:
+	index = f.read()
+
+while True:
+	with open("index.html", "r", encoding="utf-8") as f:
+		_index = f.read()
+	
+	if _index != index:
+		index = _index
+		
+		pages = {
+			'./Solar-System.html': 'solar_system',
+			'./Planet-Gaia-Derinkuyu.html': 'planet',
+			'./Life-on-planet.html': 'life',
+			'./About.html': 'about',
+			'./Team.html': 'team',
+		}
+		for page in pages:
+			# print(os.path.abspath(page))
+			with open(page, "w", encoding="utf-8") as f:
+				f.write(index)
+
+		print("Copied to others")
+
+	time.sleep(1)
